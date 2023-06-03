0287167189eb5ec0bc2cef944e793e1511a0e9ab
Wojciech <ficwojciech95@gmail.com>
2023-06-03 10:36:33 +0200

    added styles burger-btn, js script

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelector('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
@@ -22,5 +25,22 @@ const handleNavItemsAnimation = () => {
 		delayTime++
 	})
 }
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+const handleObserver = () => {
+	const currentSection = window.scrollY
 
+	allSections.forEach(section => {
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+	})
+}
+
+handleCurrentYear()
 navBtn.addEventListener('click', handleNav)
+window.addEventListener('scroll', handleObserver)
